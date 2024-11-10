8bb98d7ac41ad8b9e8e2734535b1675aff3ba3aa
chltndid724 <chltndid724@gmail.com>
2024-11-10 15:27:27 +0900

    1110

---
+++ src/test/java/io/cucumber/ConfigManager.java
@@ -0,0 +1,39 @@
+package io.cucumber;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigManager {
+    private static ConfigManager instatnce;
+
+    private Properties properties;
+
+    private ConfigManager() {
+        properties = new Properties();
+        loadProperties();
+    }
+
+    public static ConfigManager getInstance() {
+        if (instatnce == null) {
+            synchronized (ConfigManager.class) {
+                if (instatnce == null) {
+                    instatnce = new ConfigManager();
+                }
+            }
+        }
+        return instatnce;
+    }
+
+    private void loadProperties() {
+        String configFilePath = "src/test/resources/webconfig.properties";
+        try (FileInputStream inputStream = new FileInputStream(configFilePath)) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
