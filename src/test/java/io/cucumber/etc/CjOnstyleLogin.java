e0763deaad3d9c57fd87dd913a7f5abb08832411
chltndid724 <chltndid724@gmail.com>
2024-11-30 21:06:17 +0900

    schedule renewff

--- src/test/java/io/cucumber/etc/CjOnstyleLogin.java
+++ src/test/java/io/cucumber/etc/CjOnstyleLogin.java
@@ -11,7 +11,7 @@
 //import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.chrome.ChromeOptions;
-//
+//d
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 //
 //public class CjOnstyleLogin {
